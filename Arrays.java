dbb69626d22650a934487d4f995d5890a628487b
Olival <olivalpaulinocc@gmail.com>
2023-01-26 20:49:15 -0300

    atualizando comentarios

--- Arrays.java
+++ Arrays.java
@@ -1,9 +1,15 @@
 package br.com.tabelaverdade;
-
+/*
+Classe resposavel por criar uma tabela verdade utilizando os conceitos de array bidimensionais/multidimisionais
+Criando uma matriz mxn, para representar e apresentar a tabela verdade da lógica booleana
+Autor: Olival Paulino
+*/
 public class Arrays {
 
     public static void main(String[] args) {
-        int[][] matriz = new int[4][7];
+        int[][] matriz = new int[4][7]; // declaracao da matriz
+        
+        // atribuindo valores nas posicoes correspondentes
         matriz[0][0] = 1;matriz[1][0] = 1;matriz[0][1] = 1;matriz[2][1] = 1;
         matriz[0][2] = 0;matriz[1][2] = 0;matriz[2][2] = 1;matriz[3][2] = 1;
         matriz[0][3] = 1;matriz[1][3] = 0;matriz[2][3] = 0;matriz[3][3] = 0;
@@ -11,9 +17,12 @@ public class Arrays {
         matriz[0][5] = 1;matriz[1][5] = 0;matriz[2][5] = 1;matriz[3][5] = 1;
         matriz[0][6] = 1;matriz[1][6] = 0;matriz[2][6] = 0;matriz[3][6] = 1;
         
+        // preparando a apresentacao de dados no formato de tabela com prinf
         System.out.println("Tabela Verdade\n----------------------------------------------------------------------");
         System.out.printf("    %s    |    %s    |   %s    |  %s  |  %s  |  %s | %s |", "p", "q", "~p", "p ^ q", "p v q", "p -> q", "p <-> q");
         System.out.println("");
+        
+        // percorrendo a matriz e apresentando os valores de cada elemento
         for (int i=0; i<4; i++) {
             for (int j=0; j<7; j++) {
                 System.out.printf("    %d    |", matriz[i][j]);
--- Arrays2.java
+++ Arrays2.java
@@ -1,16 +1,18 @@
 package br.com.tabelaverdade;
 
 /**
- *
+ * Criando uma matriz com três linhas e quantidade diferente de colunas.
+ * Acessando cada elemento da matriz, as colunas, através da leitura da linha
  * @author Olival Paulino
  */
 public class Arrays2 {
+    
     public static void main(String[] args) {
         int[][] array = {{1,2}, {3}, {4,5,6}};
         
-        // quantidade de linhas
+        // Percorre a quantidade de linhas
         for (int i = 0; i < array.length; i++) {
-            // quantidade de colunas
+            // Percorre a quantidade de colunas
             for (int j = 0; j < array[i].length; j++) {
                 System.out.printf("%d ",array[i][j]);
             }
